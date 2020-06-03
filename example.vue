5bd17545e7e013f6ad75f8b2d6013cdea18705b1
Hb_zzZ <31434560+Hb-zzZ@users.noreply.github.com>
2020-06-03 17:49:00 +0800

    Create example.vue

---
+++ example.vue
@@ -0,0 +1,19 @@
+// 伪代码
+<template>
+  <custom-iframe :styles="styles">
+    <vue-component />
+  </custom-iframe>
+</template>
+
+----
+<script>
+import iframeStyles from '!raw-loader!sass-loader!./iframeStyles.css'
+
+export default {
+  data() {
+    return {
+       styles: iframeStyles
+    }
+  }
+}
+</script>
