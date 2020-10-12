ec6dd2c408559619b8038c226810f631a1a3a9a4
Hb-zzZ <Hb-email@foxmail.com>
2020-10-12 14:14:53 +0800

    🏗  Building-construction: 提供更详细的demo
    "re #1"

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <button @click="handleIncrement">更改vuex状态</button>
+    下面是iframe
+    <custom-iframe
+      :styles="iframeStyles"
+      style="border: 1px solid;min-height: 400px;"
+    >
+      ifrmae区域
+      <img src="./assets/logo.png" alt="vue logo" style="display: block;" />
+      <custom-title title="自定义组件" />
+      vuex-count：{{ count }}
+    </custom-iframe>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+import CustomIframe from './components/CustomIframe.vue'
+import CustomTitle from './components/CustomTitle.vue'
+import iframeStyles from '!raw-loader!sass-loader!./iframeStyles.scss'
+
+export default {
+  name: 'App',
+  components: {
+    CustomIframe,
+    CustomTitle
+  },
+  data() {
+    return {
+      iframeStyles
+    }
+  },
+  computed: {
+    ...mapState({
+      count: (state) => state.count
+    })
+  },
+  methods: {
+    handleIncrement() {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/CustomTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2
+    style="border-left: 3px solid rosybrown;
+  padding-left: 16px;"
+  >
+    {{ title }}
+  </h2>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
